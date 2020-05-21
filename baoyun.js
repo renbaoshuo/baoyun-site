809ab2983db125d8267d588a7ad9e23d25d80fdb
任宝硕 <renbaoshuo@qq.com>
2020-05-21 11:53:51 +0800

    Add Something

---
+++ baoyun.js
@@ -0,0 +1,60 @@
+/**
+ * Baoyun's Small Site
+ * Author: RenBaoshuo
+ * Date: 2020/05/21
+ * Link: https://www.baoshuo.ren/
+ * Github: renbaoshuo/baoyun-site
+ */
+
+// 函数定义
+var baoyuntime = function() {
+    var ndate  = new Date();
+    var ntzone = ndate.getTimezoneOffset() / 60;
+    var nyear  = ndate.getFullYear();
+    var nmonth = ndate.getMonth();
+    var nday   = ndate.getDay();
+    var nhour  = ndate.getHours();
+    var byear  = 0;
+    var bmonth = 0;
+    if(ntzone != -8) {
+        // 调整时区
+        // TO DO
+        // 2020/05/21 - 直接隐藏
+        document.getElementById('baoyun-time').style.display = 'none';
+    } else {
+        // 北京时间无需调整
+        // 计算岁
+        if((nyear > 2020) && (nmonth == 1)) {
+            byear = nday > 25 ? nyear - 2020 : nyear - 2021;
+        } else {
+            byear = nyear - 2020;
+        }
+        // 判断输出
+        if(byear > 0) {
+            document.getElementById('baoyun-year').innerHTML = byear;
+        } else {
+            document.getElementById('baoyun-year-pre').style.display = 'none';
+            document.getElementById('baoyun-year').style.display     = 'none';
+            document.getElementById('baoyun-year-des').style.display = 'none';
+        }
+        // 计算月份
+        if(ndate <= 25) {
+            bmonth = nmonth - 1;
+        } else {
+            bmonth = nmonth;
+        }
+        // 判断输出
+        if(bmonth != 0) {
+            document.getElementById('baoyun-month').innerHTML = bmonth;
+        } else {
+            document.getElementById('baoyun-month-pre').style.display = 'none';
+            document.getElementById('baoyun-month').style.dispaly     = 'none';
+            document.getElementById('baoyun-month-des').style.display = 'none';
+        }
+    }
+}
+
+// 程序入口
+baoyuntime();                 // 首次处理
+setInterval(baoyuntime,100);  // 后续更新
+
