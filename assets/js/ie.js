e643e3c9dd38c8e49941d2fc229e2f7979639ca0
任宝硕 <renbaoshuo@qq.com>
2020-05-22 16:10:37 +0800

    Update ie.js

--- assets/js/ie.js
+++ assets/js/ie.js
@@ -30,11 +30,11 @@
         '<h1>您可能在使用不受支持的<b style="color:#f00; font-size: 125%;">过时</b>浏览器，导致加载出现了问题。</h1>' +
         '<h3 style="text-align: left; font-size:15px">如果您使用国产浏览器，请在右上方选择 <b style="color:#f00; font-size: 125%;">“极速模式”</b> ，而非 “兼容模式” 。</h3>' +
         '<h3 style="text-align: left; font-size:15px">推荐使用基于 Chromium 的最新版本的 <a target="_blank" href="https://www.microsoft.com/zh-cn/windows/microsoft-edge" style="color:#f62;">Microsoft Edge 浏览器</a> 访问本站</h3>' +
-        '<div style="text-align: left !important;<span style="font-size: 65%">UA 调试区: </span><code style="padding: 2px 4px; font-size: 55%; color: #c7254e; background-color: #f9f2f4; border-radius: 4px; text-align:left">' + ua + '</code></div><br>';
+        '<div style="text-align: left !important;"><span style="font-size: 65%">UA 调试区: </span><code style="padding: 2px 4px; font-size: 55%; color: #c7254e; background-color: #f9f2f4; border-radius: 4px; text-align:left">' + ua + '</code></div><br>';
 
     var elem = document.getElementById('main');
     window.browser_orzed = orzed;
     if (orzed) {
         elem.innerHTML = infoHTML;
-    };
+    }
 })();
